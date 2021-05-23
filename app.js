7523cd238b91d35e573c289e0cb79350149b98ba
sam010181 <84663727+sam010181@users.noreply.github.com>
2021-05-24 03:58:00 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
 server.listen(port,() => {
   console.log(`Server running at port `+port);
 });
