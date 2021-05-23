52d5289da95b60c357fd3a73496da9263c705335
sam010181 <84663727+sam010181@users.noreply.github.com>
2021-05-24 03:55:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const http = require('http');
 const port = process.env.PORT || 3000
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
-});
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 server.listen(port,() => {
   console.log(`Server running at port `+port);
