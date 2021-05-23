d63be3a94b86614006d10bdac720bb6c91cabeab
sam010181 <84663727+sam010181@users.noreply.github.com>
2021-05-24 04:11:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/',flower.getRecipe )
+app.get('/',flower.getRecipe);
 
 app.listen(port,() => {
   console.log(`Server running at port `+port);
